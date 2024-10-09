ba061d30539735e31d82f33a96f01d1e7b23a51f
theresatree <1a1.nghoeping@gmail.com>
2024-10-10 01:10:32 +0800

    a

--- sc2002/src/main/java/sc2002/UserDB.java
+++ sc2002/src/main/java/sc2002/UserDB.java
@@ -1,5 +1,49 @@
 package sc2002;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+
 public class UserDB {
-    
+    private final String filePath = "sc2002/src/main/resources/User.xlsx"; //fixed file location for User.xlsx
+
+    public boolean validateLogin(String hospitalID, String password) throws IOException {
+        try {
+            InputStream is = new FileInputStream(filePath);
+        }
+        catch (FileNotFoundException e){
+            throw new IOException("File not found in resources or local path: " + filePath);
+        }
+
+        Workbook workbook = new XSSFWorkbook(is);
+        Sheet sheet = workbook.getSheetAt(0);
+        boolean isValid = false;
+        for (Row row : sheet) {
+            Cell idCell = row.getCell(0);
+            Cell passwordCell = row.getCell(1);
+            // Skip header row
+            if (row.getRowNum() == 0) continue;
+            if (idCell != null && passwordCell != null) {
+                String id = idCell.getStringCellValue();
+                String pass = passwordCell.getStringCellValue();
+
+                if (id.equals(hospitalID) && pass.equals(password)) {
+                    isValid = true;
+                    break;
+                }
+            }
+        }
+        workbook.close();
+        is.close();
+        return isValid;
+    }
 }
