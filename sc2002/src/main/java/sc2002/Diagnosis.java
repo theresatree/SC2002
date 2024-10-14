2f48a54eeaada3c84e12ab7c9d092a255939c459
theresatree <1a1.nghoeping@gmail.com>
2024-10-14 14:30:29 +0800

    Redid MedicalRecord

--- sc2002/src/main/java/sc2002/Diagnosis.java
+++ sc2002/src/main/java/sc2002/Diagnosis.java
@@ -5,24 +5,27 @@ public class Diagnosis {
     private int diagnosisCode;
     private String doctorID;
     private String descriptionOfDiagnosis;
+    private String descriptionOfTreatmtent;
     private String dateOfDiagnosis;
     private String additionalNotes;
 
-    Diagnosis(String patientID, int diagnosisCode, String doctorID, String descriptionOfDiagnosis, String dateOfDiagnosis, String additionalNotes) {
+    Diagnosis(String patientID, int diagnosisCode, String doctorID, String descriptionOfDiagnosis, String descriptionOfTreatment, String dateOfDiagnosis, String additionalNotes) {
         this.patientID = patientID;
         this.diagnosisCode = diagnosisCode;
         this.doctorID = doctorID;
         this.descriptionOfDiagnosis = descriptionOfDiagnosis;
+        this.descriptionOfTreatmtent = descriptionOfTreatment;
         this.dateOfDiagnosis = dateOfDiagnosis;
         this.additionalNotes = additionalNotes;
     }
 
     public String printDiagnosis(){
         return(
-            "Diagnosis code: " + diagnosisCode + "\n" +
+            "Diagnosis ID: " + diagnosisCode + "\n" +
             "Date: " + dateOfDiagnosis + "\n" +
             "By Doctor ID: " + doctorID + "\n" +
             "Diagnosis: " + descriptionOfDiagnosis + "\n" +
+            "Treatment: " + descriptionOfTreatmtent + "\n" +
             "Additional notes: " + additionalNotes);
     }
 }
